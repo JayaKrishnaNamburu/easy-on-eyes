f742fb06a39984eee9d87e0c224fb068cd05a24f
JayaKrishna <namburu1995@gmail.com>
2018-12-12 21:49:29 +0530

    added the ability to change colours

--- magic.js
+++ magic.js
@@ -1,1 +1,26 @@
-console.log('magic is happeneing');
+(function magic() {
+    const scrollArea = document.getElementById('convArea-scrollarea');
+    setStyle(scrollArea);
+    const container = document.getElementById('container');
+    setStyle(container);
+    const msgItem = document.getElementsByClassName('body');
+    const msgText = document.getElementsByClassName('markdown-body');
+    Array.from(msgText).forEach((item) => item.style.color = "#ffffff");
+    Array.from(msgItem).forEach((item) => {
+        item.parentElement.style.backgroundColor = "#6E6E6E";
+    });
+    const msgArea = document.getElementById('message-area');
+    msgArea.style.backgroundColor = "#040404";
+    msgArea.parentElement.style.backgroundColor = "#040404";
+    const inputArea = document.getElementsByClassName('messageArea');
+    inputArea[0].style.backgroundColor = "#040404 !important";
+    inputArea[0].style.background = "#040404 !important";
+})()
+
+function setStyle(el) {
+    el.setAttribute("style", "background-color: #040404");
+}
+
+function setParentStyle(el) {
+    el.parent.setAttribute("style", "background-color: #040404");
+}
--- popup.js
+++ popup.js
@@ -1,9 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
+    chrome.tabs.onUpdated.addListener(function(tabId, change, tab) {
+        if (change.status === "complete" && tab.url.includes('')) {
+            chrome.tabs.executeScript(tabId, { file: 'magic.js' });
+        }
+    });
     const el = document.getElementById('force');
     el.addEventListener('click', function() {
         chrome.tabs.getSelected(null, function(tab) {
-            if (tab.url.includes('talk.fresco')) {
-                chrome.tabs.executeScript(null, { file: 'magic.js' });
+            if (tab.url.includes('')) {
+                chrome.tabs.executeScript(tab.id, { file: 'magic.js' });
             } else {
                 alert('Sorry plugin is not supported');
             }
